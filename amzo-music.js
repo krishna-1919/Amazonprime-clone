6ec69074f983f087c5e519304032ed60db2248ce
krishna <ktishnabamne@gmail.com>
2023-08-29 10:38:41 +0530

    new project

---
+++ amzo-music.js
@@ -0,0 +1,27 @@
+// let n2= document.querySelector(".n2");
+// let n3= document.querySelector(".n3");
+// let n4= document.querySelector(".n4");    
+
+// n2.addEventListener("mouseenter",function(){
+//     this.children[1].style.color="red";
+// });
+
+
+// // n3.addEventListener("click",colorchange2());
+// // n4.addEventListener("click",colorchange3());
+
+// setTimeout(()=>{
+//     let body=document.querySelector(".mainer");
+//     body.style.marginTop="-1500px";
+// },1000) 
+
+setTimeout(()=>{
+    let b=document.querySelector(".mainer");
+    b.style.marginTop="0px";
+},1000)    
+
+setTimeout(()=>{
+    let a=document.querySelector(".animated");
+    a.style.marginTop="-15000px";
+
+},2000)
